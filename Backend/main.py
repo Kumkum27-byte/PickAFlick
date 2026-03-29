771335b93bbbd01f5356a7ce6ad3e0cc65232ace
Kumkum27-byte <kumkum.morewal27@gmail.com>
2026-03-29 17:31:35 +0530

    Add CORS middleware

--- Backend/main.py
+++ Backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500",
+    allow_origins=["https://pick-a-flick-rose.vercel.app/",
     "http://127.0.0.1:5501",
     "http://localhost:5500",
     "http://localhost:5501"],
